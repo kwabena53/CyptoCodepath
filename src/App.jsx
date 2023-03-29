dd9fb593af31f2cb05f15688f90d05bb61a12312
Kwabena Adu-Darkwa <kwabenaadudarkwa@gmail.com>
2023-03-28 18:56:50 -0700

    scommit

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,23 @@ const API_KEY = import.meta.env.VITE_APP_API_KEY;
 
 function App() {
 
-  const [list, setList] = useState(null)
+  const [list, setList] = useState(null);
   const [filteredResults, setFilteredResults] = useState([]);
   const [searchInput, setSearchInput] = useState("");
 
- console.log(list)
   useEffect(() => {
 
     const fetchAllCoinData = async () => {
-      const query = "https://min-api.cryptocompare.com/data/all/coinlist?&api_key"+ API_KEY
-      const response = await fetch(query);
+      const response = await fetch("https://min-api.cryptocompare.com/data/all/coinlist?&api_key" + API_KEY);
       const json = await response.json();
+      console.log(json);
       setList(json);
     };
 
+    //Calls functions and handles error
     fetchAllCoinData().catch(console.error);
-  }, []);
 
+  }, []);
 
   const searchItems = searchValue => {
     setSearchInput(searchValue);
@@ -39,53 +39,41 @@ function App() {
     }
   };
   
-
   return (
     <div className="whole-page">
-  <h1>My Crypto List</h1>
-  <input
-    type="text"
-    placeholder="Search..."
-    onChange={(inputString) => searchItems(inputString.target.value)}
-  />
-      <ul>
-          {/* 
-          
-          {list && Object.entries(list.Data).map(([coin]) =>
-          list.Data[coin].PlatformType === "blockchain" ? (
-            <CoinInfo
-            key={list.Data[coin].id}
-            image={list.Data[coin].ImageUrl}
-            name={list.Data[coin].FullName}
-            symbol={list.Data[coin].Symbol}
-            />
-        ) : null
-    )} 
-    */}
+      {/*<SideNav/>  remove sidenav due to api not working*/}
+      <h1>My Crypto List</h1>
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(inputString) => searchItems(inputString.target.value)}
+        />
+        <ul>
+          {searchInput.length > 0
+          ? filteredResults.map((coin, index) => 
+                list.Data[coin].PlatformType === "blockchain" ? 
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                  key={index}
+                />
+                : null
+            )
+          : list && Object.entries(list.Data).map(([coin], index) => 
+                list.Data[coin].PlatformType === "blockchain" ? 
+                <CoinInfo
+                  image={list.Data[coin].ImageUrl}
+                  name={list.Data[coin].FullName}
+                  symbol={list.Data[coin].Symbol}
+                  key={index}
+                />
+                : null
+             )}
+        </ul>
+        
 
-    {searchInput.length > 0
-          ? // what happens if we have search input? what list do we use to display coins?   
-          filteredResults.map((coin)=>(
-            <CoinInfo
-            key={coin.id}
-            image={coin.ImageUrl}
-            name={coin.FullName}
-            symbol={coin.Symbol}
-            /> 
-            // console.log(coin)
-          ))
-            
-          : // what happens if we don't have search input? what list do we use to display coins? 
-          list && Object.entries(list.Data).map(([coin]) =>list.Data[coin].PlatformType === "blockchain" ? (
-            <CoinInfo
-            key={list.Data[coin].id}
-            image={list.Data[coin].ImageUrl}
-            name={list.Data[coin].FullName}
-            symbol={list.Data[coin].Symbol}
-            />): null )         
-          } 
-      </ul>
-</div>
+    </div>
   )
 }
 
---
+++ src/routes/DetailView.jsx
@@ -0,0 +1,14 @@
+
+import CoinDetail from "../Components/coinDetail";
+
+const DetailView = () => {
+    return (
+      <div>
+        
+        <CoinDetail />
+      </div>
+    );
+  };
+  
+  export default DetailView;
+
